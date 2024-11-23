47a97d76332b7bb8bc41c0edb10fab3a2ed7a3e8
ZeeshanRamzan06 <zeeshanramzan3f@gmail.com>
2024-11-24 00:42:10 +0500

    adding middleware and utils

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ dotenv.config(
         path: './env'
     })
 
-connectDB();
-
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is running at port : ${process.env.PORT} `);
+    } )
+})
+.catch(
+    (error)=>{
+        console.log("MonoDB Connection failed .. !! !!",error)
+    }
+)
 /*
 import express from "express"
 const app =express()
